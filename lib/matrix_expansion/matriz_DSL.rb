a2514051e635bd35b00df759b34dda248c0446d5
OpenShift guest <tenoglez@gmail.com>
2013-12-10 14:33:32 -0500

    Creado en matriz_DSL, el initialize que permite definir las diferentes opciones y modos de ejecución. Aún sin implementar.

--- lib/matrix_expansion/matriz_DSL.rb
+++ lib/matrix_expansion/matriz_DSL.rb
@@ -19,24 +19,49 @@
 #        Definicion de la clase Matriz_DSL compuesta por:
 #
 
-#modulo que agrupa la resolucion de la practica 12
-module prct12
-	module Operaciones
-    	SUMA =            :suma
-  		RESTA =           :resta
-  		MULTIPLICACION =  :multiplicacion
-  		DIVISION =        :division
-	end
+require "./lib/math_expansion/matriz.rb"
+require "./lib/math_expansion/matriz_dispersa.rb"
+require "./lib/math_expansion/matriz_densa.rb"
+require "./lib/math_expansion/matriz_operaciones.rb"
 
-	module Output
-  		CONSOLE = :console
-  	end
-
-  	class Matriz_DSL
-  		
-
-  	end
+module MathExpansion
+    class MatrizDSL < Matriz
+        attr_accessor :result
+        
+        def initialize(op_type, &block)
+            raise ArgumentError , 'Tipo invalido' unless op_type.is_a? String
+            
+            @operands = []
+            @result_mode = :auto
+            @result = nil
+            @mode = :console
+            @operation = :mostrar
+            
+            
+            case op_type
+                when "suma"
+                    @operation = :suma
+                when "resta"
+                    @operation = :resta
+                when "producto"
+                    @operation = :producto
+                when "mostrar"
+                    @operation = :mostrar
+                else
+                    puts "Opción incorrecta", op_type
+            end
+            
+            if block_given?  
+                if block.arity == 1
+                    yield self
+                else
+                    instance_eval &block 
+                end
+            end
+            
+            ejecucion
+        end
+    
+    end
 end
-
-
-
+    
